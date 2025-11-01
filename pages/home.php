799dc41f1c2e3c7dbf0668ae3523961abc4fff36
Kkristina82 <grigir7kristina@gmail.com>
2025-11-01 23:17:31 +0200

    Task №1

--- pages/home.php
+++ pages/home.php
@@ -1,37 +1,273 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= htmlspecialchars($title ?? 'Сторінка', ENT_QUOTES, 'UTF-8') ?></title>
-    <link rel="stylesheet" href="./styles/style.css">
-    <script src="./scripts/script.js"></script>
-    <!-- Favicon базовий -->
-    <link rel="icon" href="./images/icons/favicon.ico" sizes="any">
+<html lang="uk">
+ <head> 
+ <meta charset="UTF-8">
+ <title><?php echo $title; ?></title>
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="/styles/style.css">
+<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+ </head>
+ <body>
+	<script src="scripts/script.js"></script>
+	<div class="wrapper">
+	<header>
+		<section class="header-seo">
+			<div class="logo">
+				<a href="file:///C:/Users/%D0%94%D0%BE%D0%BC%D0%BE%D0%B2%D0%BE%D0%B9/Desktop/web-%D0%B4%D0%B8%D0%B7%D0%B0%D0%B9%D0%BD/index.html">
+				<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Sweet.tv_logo.svg/2560px-Sweet.tv_logo.svg.png" alt="sweet.tv" width="140" height="26"></a>
+			</div>
+			<div class="routes">
+				ТБ Безкоштовне ТБ Фільми Мультики Серіали Приставка Тарифи 
+				<div class="uk" > 
+					uk   
+				</div> 
+			<div class="search">   
+			<img src="https://sweet.tv/images/icons/search.svg" alt="" width="24" height="24" >
+			</div>
+			<div class="Log_in">
+				<a href="http://localhost/login" class="Log_in">Увійти</a>
+			</div>
+				
+			<div class="free">
+				<button><a href="http://localhost/login" class="Log">Спробувати безкоштовно</a></button>                   
+			</div>
+		</div>
+		</section>
+	</header>
+	<main>
+	<div class="content">
+			<h1>Світові новинки кіно<br>та телебачення на<br><strong>SWEET.TV</strong></h1>
+      <button><a href="http://localhost/logn" class="Log">Підключити безкоштовні 7 днів</a></button>
+		</div>
+		<div class="tv-screen">
+			<img src="https://sweet-tv-static.sweet.tv/web/v2/home/block_2-UA-en-1729173052.png" alt="" width="550" height="400">
+		</div>
+		
+		<div class="interface">
+			<img src="https://sweet-tv-static.sweet.tv/web/v2/main_page/icons/flash.svg" alt="tablet" class="main-page">
+			Просте підключення
+			<img src="https://sweet-tv-static.sweet.tv/web/v2/main_page/icons/interface.svg" alt="інтерфейс" class="main-page">
+			Зрозумілий інтерфейс
+			<img src="https://sweet-tv-static.sweet.tv/web/v2/main_page/icons/collection.svg" alt="оновлення" class="main-page">
+			Постійне оновлення
+		</div>
+		<div class="device">
+			<p class="main-page-ua__header-devices-texts-title">Дивись на будь-якому пристрої</p>
+			<p class="main-page-ua__header-devices-texts-description">
+				Підключай одночасно на 5 пристроях. Додаток можна встановити на телевізор, смартфон, ноутбук, планшет чи на Xbox
+			</p>
+			<div class="main-page-ua__header-devices-content">
+				<div class="main-page-ua__header-devices-device">
+					<img src="https://sweet-tv-static.sweet.tv/web/v2/main_page/icons/tv.svg" alt="tv" width="48" height="48" class="main-page-ua__header-devices-device-icon">
+					<p class="main-page-ua__header-devices-device-text"> телевізор</p>
+				</div>
 
-    <!-- PNG-іконки для браузерів -->
-    <link rel="icon" type="image/png" sizes="16x16" href="./images/icons/favicon-16x16.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="./images/icons/favicon-32x32.png">
+				<div class="main-page-ua__header-devices-device">
+					<img src="https://sweet-tv-static.sweet.tv/web/v2/main_page/icons/laptop.svg" alt="laptop" width="48" height="48" class="main-page-ua__header-devices-device-icon">
+					<p class="main-page-ua__header-devices-device-text"> комп'ютер</p>
+				</div>
 
-    <!-- Apple Touch Icon (для iOS / iPadOS) -->
-    <link rel="apple-touch-icon" sizes="180x180" href="./images/icons/apple-touch-icon.png">
+				<div class="main-page-ua__header-devices-device">
+					<img src="https://sweet-tv-static.sweet.tv/web/v2/main_page/icons/mobile.svg" alt="mobile" width="48" height="48" class="main-page-ua__header-devices-device-icon">
+					<p class="main-page-ua__header-devices-device-text"> телефон</p>
+				</div>
 
-    <!-- Android / PWA -->
-    <link rel="manifest" href="./images/icons/site.webmanifest">
+				<div class="main-page-ua__header-devices-device">
+					<img src="https://sweet-tv-static.sweet.tv/web/v2/main_page/icons/tablet.svg" alt="tablet" width="48" height="48" class="main-page-ua__header-devices-device-icon">
+					<p class="main-page-ua__header-devices-device-text"> планшет</p>
+				</div>
+			
 
-    <!-- Тема і колір плитки для Windows -->
-    <meta name="theme-color" content="#ffffff">
-    <meta name="msapplication-TileColor" content="#ffffff">
-</head>
-<body>
-    <h1>Головна сторінка (файл /pages/home.php)</h1>
-    <p>
-        <?php
-            echo "Привіт, " . ($_SESSION['login'] ?? 'КОРИСТУВАЧ НЕ АВТОРИЗОВАНИЙ');
-        ?>
-    </p>
 
-    <img src="https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=https%3A%2F%2Fkn24-php.onrender.com" alt="QR kn24-php">
+				</div></div>
+		<div class="SWEET_TV">
+			<div class="chat">
+			<div class="chat-icon">
+				<img src="https://www.pngmart.com/files/23/Chat-Icon-PNG-HD.png" alt="sweet.tv" width="45" height="45">              
+			</div></div>
+		<div data-aos="fade-up"> 
+				<h1>Що входить у тариф SWEET.TV? <br> 414 каналів</h1>
+			<div class="m2__what-text">
+				<div class="m2__what-item">
+					<h3>Національне та преміальне ТБ</h3>
+					<p class="gray">Дивись національні, фільмові, преміальні, дитячі, спортивні та інші телеканали</p>
+					<h3>Керуй прямим ефіром</h3>
+					<p class="gray">Перемотуй, став на паузу та продовжуй перегляд пізніше</p>
+					<h3>Передачі в записі до 7 днів</h3>
+					<p class="gray">Більше не треба поспішати, щоб встигнути на початок програми</p>
+				</div>
+			</div>
+		</div>
+		<div class="what-img">
+				<img src="https://sweet.tv/images/v2/index_2022/channels_uk.png" alt="" width="450" height="300">
+		</div>
+		<h1><br>Щомісяця світові прем'єри <br> від голлівудських студій на SWEET.TV</h1>             
+		<div class="what-item">
+			<h3>Новинки кіно одразу після кінотеатру</h3>
+			<p class="gray">від Disney, Paramount, Sony, Universal, Warner Bros</p>
+			<h3>Найкращі фільми світу</h3>
+			<p class="gray">з рейтингом 7–10 за IMDb</p>
+			<h3>Прем'єри українського кіно</h3>
+			<h3>Ексклюзивний україномовний дубляж</h3>
+			<h3>Всі фільми без реклами</h3>
+		</div>	
 
+		<div class="Photo" >
+						<img src="https://static.sweet.tv/images/cache6/posters/BDOO6AISAJ2WWKQCBABA====/298443-dedpul-i-rosomaha.jpg" width="154" height="229" alt="" >
+			<img src="https://static.sweet.tv/images/cache6/posters/BCZO4AISAJ2WWKQCBABA====/298305-zaberi-mene-na-misyac.jpg" width="154" height="229" alt="">
+			<img src="https://static.sweet.tv/images/cache6/posters/BC7OOAISAJ2WWKQCBABA====/284158-korolivstvo-planeti-mavp.jpg"  width="154" height="229" alt="">
+			<img src="https://static.sweet.tv/images/cache6/posters/BC7LCAISAJ2WWKQCBABA====/22718-raya-ta-ostanniy-drakon.jpg"  width="154" height="229" alt="">
+			<img src="https://static.sweet.tv/images/cache6/posters/BCYOYAISAJ2WWKQCBABA====/298248-ebigeyl.jpg" width="154" height="229" alt="">
+			<img src="https://static.sweet.tv/images/cache6/posters/BCO6GAISAJ2WWKQCBABA====/298353-smerchi.jpg"  width="154" height="229" alt="">
+			<img src="https://static.sweet.tv/images/cache6/posters/BCIOKAISAJ2WWKQCBABA====/298296-tihe-misce-den-pershiy.jpg"  width="154" height="229" alt="">
+			<img src="https://static.sweet.tv/images/cache6/posters/BC26KAISAJ2WWKQCBABA====/284167-garfild-u-kino.jpg" width="154" height="229" alt="">
+		</div>
+
+		
+		<div class="m2__studios">
+					<img src="https://sweet.tv/images/v2/index_2022/dreamworks.svg" width="95" height="56" alt="">
+					<img src="https://sweet.tv/images/v2/index_2022/disney.svg" width="115" height="41" alt="">
+					<img src="https://sweet.tv/images/v2/index_2022/sony.svg" width="40" height="61" alt="">
+					<img src="https://sweet.tv/images/v2/index_2022/warner.svg" width="52" height="53" alt="">
+		</div>   
+		<section class="Cartoon" >
+				<h1>Мультфільми</h1>
+				<div class="m3__what-item">
+				<p class="gray">Шедеври мультиплікації від Disney, Pixar, DreamWorks та ін. для дітей будь-якого віку та для всієї<br> родини. Дивись українською та без реклами: “Свинка Пеппа”, “Нікчемний Я”, “Рататуй”, “Шрек” та ін.</p>
+				</div>
+
+			<div class="m2__what-wrapper">	
+				<img src="https://sweet.tv/images/v2/index_2022/mult_bird.png" alt="" class="parrot" >
+				<div class="m2__what-tablet">
+					<video class="lozad"   style="background-image: url(&quot;https://sweet.tv/images/v2/index_2022/mult_tablet.png&quot;);">
+						<source src="https://sweet-tv-static.sweet.tv/video/main_1/cartoons.mp4" type="video/mp4">
+					</video>
+				</div>
+			</div>
+		</section>
+
+		<div class="m2__studios">
+			<img src="https://sweet.tv/images/v2/index_2022/dreamworks.svg" width="95" height="56" alt="">
+			<img src="https://sweet.tv/images/v2/index_2022/disney.svg" width="115" height="41" alt="">
+			<img src="https://sweet.tv/images/v2/index_2022/sony.svg" width="40" height="61" alt="">
+			<img src="https://sweet.tv/images/v2/index_2022/warner.svg" width="52" height="53" alt="">
+		</div> 		
+		<div class="sports-section">
+			<div class="sports-text">
+				<h2>Спорт</h2>
+				<h3>Європейський футбол</h3>
+				<p class="gray">Трансляції футбольних чемпіонатів Англії, Німеччини та Бельгії.</p>  
+				<h3>Спортивні події</h3>
+				<p class="gray">Яскраві спортивні події зі світу UFC, NHL, NBA, MLB, Formula 1<br> і Formula E, турніри з тенісу, снукеру, гольфу та ін.</p>
+			</div>
+			<div class="sports--wrapper">
+				<img src="https://sweet.tv/images/v2/index_2022/sport.png" alt="sport" class="sports-video">
+			</div>
+		</div>	
+			<div class="rozetka" >
+				<h1>Огляд SWEET.TV від ROZETKA</h1>
+				<p class="gray">Незалежні експерти з ROZETKA оцінили SWEET.TV та розповіли про сервіс на своєму каналі у YouTube</p>
+					<iframe  src="https://www.youtube.com/embed/ag21zj6G1no" width="700" height="380" >
+					</iframe>
+		</div>
+		<section class="gift-promo" data-background-image="https://sweet.tv/images/v2/index_2022/bg-subscr.jpg" data-loaded="true" style="background-image: url(&quot;https://sweet.tv/images/v2/index_2022/bg-subscr.jpg&quot;);">
+			<div class="m2__subsrc_lozad">
+					<h2> Даруємо 7 днів <br>передплати</h2>
+					<h3>Дивись ТБ і кіно 7 днів без обмежень</h3>
+				<div class="free_7">	
+					<p>Отримати в подарунок <br> передплату на 7 днів</p>            
+				</div>
+			</div>
+		</section>
+
+		<section class="m2__sveet">
+			<div class="m2__people m2__left-people">
+				<img src="https://sweet.tv/images/v2/index_2022/people_left.png" width="490" height="440" alt="people">
+			</div>
+			<div class="m2_logo">
+				<img src="https://sweet.tv/images/v2/index_2022/logo_full.svg" alt="" width="245" height="71">
+			</div>
+			<div class="m2__count">
+				<span>2</span>
+				<span>5</span>
+				<span>6</span>
+				<span>0</span>
+				<span>6</span>
+				<span>2</span>
+				<span>3</span>
+				<p class="gray">Людей вже спробувало SWEET.TV</p>
+			</div>
+			<div class="m2__people m2__right-people">
+				<img src="https://sweet.tv/images/v2/index_2022/people_right.png" width="490" height="440" alt="people">
+			</div>
+		</section>
+		<div class="m2__sound-img">
+			<img class="img_wauto_hauto lozad" width="950" height="578" src="https://sweet.tv/images/v2/index_2022/microphone.png"  alt="">
+		</div>
+		<p class="h1_title">Насолоджуйся<br> ексклюзивним<br> українським дубляжем </p>
+		<p class="gray">SWEET.TV власними коштами створює авторський дубляж і озвучку для <br>культових голлівудських кінохітів</p>
+
+
+		<section class="m2__family-block">
+			<div class="h2_title">
+				<h1>Заощаджуй!<br>Одна передплата для <br>всієї родини</h1>
+				<div class ="gray_1 " >
+				Підключай батьків та близьких до однієї передплати. Активуй SWEET.TV <br> одночасно на 5 пристроях. Програма доступна на телевізорах,<br> смартфонах, ноутбуках, планшетах і навіть на Xbox</div>
+				<nav class="m2__family-apps">
+					<a href="https://play.google.com/store/apps/details?id=tv.sweet.player" >
+						<span class="google_play"></span>
+						<img src="https://sweet.tv/images/v2/index_2022/google_dark.svg" alt="google">
+					</a>
+					<a href="https://apps.apple.com/UA/app/sweet-tv/id1356560199">
+						<span class="app_store"></span>
+						<img src="https://sweet.tv/images/v2/index_2022/apple_dark.svg" alt="apple">
+					</a>
+					<a href="https://sweet.tv/faq" target="_blank">
+						<span class="smart_tv" style="padding-left: 10px;"></span>
+						<img src="https://sweet.tv/images/v2/index_2022/smart_dark.svg" alt="smart tv">
+					</a>
+					<a href="https://appgallery.huawei.com/#/app/C101034629" >
+						<span class="app_gallery"></span>
+						<img src="https://sweet.tv/images/v2/index_2022/huawei_dark.svg" alt="huawei">
+					</a>
+				</nav>
+			</div>
+		</section>
+		</div>
+	</main>
+	<footer class="footer">
+		<div class="column">
+		<p><a>ТБ</a></p>
+		<p><a>Фільми</a></p>
+		<p><a>Мультики</a></p>
+		<p><a>Серіали</a></p>
+		<p><a>Тарифи</a></p>
+		</div>
+		<div class="column">
+		<p><a>Навчання</a></p>
+		<p><a>Добірки</a></p>
+		<p><a>Новини</a></p>
+		<p><a>Промокод</a></p>
+		</div>
+		<div class="column">
+		<p><a>Про нас</a></p>
+		<p><a>Hollywood українською</a></p>
+		<p><a>Підтримка IT</a></p>
+		<p><a>Фонд кіно</a></p>
+		<p><a>Для преси</a></p>
+		</div>
+		<div class="column">
+		<p><a>Партнери</a></p>
+		<p><a>Контакти</a></p>
+		<p><a>Договір</a></p>
+		<p><a>Мапа сайту</a></p>
+		<p><a>Політика конфіденційності</a></p>
+		<p><a>Ідентифікатор медіа</a></p>
+		<p><a>Структура власності</a></p>
+		</div>
+	</footer>
+	</div>
+<script src="/scripts/script.js"></script>
 </body>
-</html>
+	</html>
+
